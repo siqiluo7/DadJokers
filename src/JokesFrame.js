9539a537078530f345c28fdf6770e9b079e36614
SIQI LUO <siqiluo@SIQIs-MacBook-Pro.local>
2019-07-21 21:43:39 -0400

    install axios

---
+++ src/JokesFrame.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+
+
+class JokesFrame extends Component{
+
+    render() {
+        return (
+             <div>
+
+             </div>
+        );
+    }
+}
+
+export default JokesFrame;
