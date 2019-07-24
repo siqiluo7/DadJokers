22383021c3377baa685a827434180ede007acc8a
SIQI LUO <siqiluo@SIQIs-MacBook-Pro.local>
2019-07-23 20:26:48 -0400

    add headers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import JokesFrame from './JokesFrame'
+import { async } from 'q';
 
 function App() {
+  
   return (
     <div className="App">
-     
+     <JokesFrame />
     </div>
   );
 }
--- src/JokesFrame.js
+++ src/JokesFrame.js
@@ -5,6 +5,12 @@ import axios from 'axios';
 
 class JokesFrame extends Component{
 
+
+    async componentDidMount(){
+          let reponse=await axios.get("https://icanhazdadjoke.com/",{headers:{Accept:"application/json"}});
+          console.log(reponse);
+
+    }
     render() {
         return (
              <div>
